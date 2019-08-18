4328f33daa1bbbf6c575b705b1bab70ed30db6f8
chenzhiyuan <imczy@vip.qq.com>
2019-08-19 01:27:33 +0800

    add celery tasks

---
+++ autotest/logconfig.py
@@ -0,0 +1,34 @@
+import logging.config
+
+LOG_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            # 'datefmt': '%m-%d-%Y %H:%M:%S'
+            'format': '%(asctime)s \"%(pathname)s：%(module)s:%(funcName)s:%(lineno)d\" [%(levelname)s]- %(message)s'
+        }
+    },
+    'handlers': {
+        'celery': {
+            # 'level': 'INFO',
+            # 'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'DEBUG',
+            'formatter': 'simple',
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': 'logs/apitest.log',
+            'when': 'midnight',
+            'encoding': 'utf-8',
+        },
+    },
+    'loggers': {
+         'apitest': {
+            'handlers': ['celery'],
+            'level': 'INFO',
+            'propagate': True,
+         }
+    }
+}
+
+logging.config.dictConfig(LOG_CONFIG)
+
