2a927463863a0059b46e90898dd732ebddf7d2d6
chenzhiyuan <imczy@vip.qq.com>
2019-08-25 12:59:43 +0800

    add celery tasks

--- celery_tasks/main.py
+++ celery_tasks/main.py
@@ -7,10 +7,10 @@ if not os.getenv('DJANGO_SETTINGS_MODULE'):
     os.environ['DJANGO_SETTINGS_MODULE'] = 'autotest.settings'
 
 # 创建celery对象,并起别名
-celery_app = Celery('celery_tasks',include=['celery_tasks.apitest.tasks'])
+celery_app = Celery('celery_tasks')
 
 #从配置文件加载配置
 celery_app.config_from_object('celery_tasks.config')
 
-#此时用定时任务来触发celery任务，因此不需要再注册celery任务
-#celery_app.autodiscover_tasks(['celery_tasks.mail', 'celery_tasks.sms','celery_task.apitest'])
+#注册celery任务
+celery_app.autodiscover_tasks(['celery_tasks.mail', 'celery_tasks.sms','celery_tasks.apitest'])
