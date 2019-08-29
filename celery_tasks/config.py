ec51f404d704a8750a5f894b6c346aea0cb94b44
chenzhiyuan <imczy@vip.qq.com>
2019-08-29 23:34:03 +0800

    add celery tasks

--- celery_tasks/config.py
+++ celery_tasks/config.py
@@ -7,9 +7,15 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # 指定任务队列的位置
-BROKER_URL = "amqp://guest@localhost:5672//"
+# rabbitMQ
+# BROKER_URL = "amqp://guest@localhost:5672//"
+# redis
+BROKER_URL = "redis://127.0.0.1/0"
 # 指定消息执行结果的位置
-CELERY_RESULT_BACKEND = "amqp://guest@localhost:5672//"
+# rabbitMQ
+#CELERY_RESULT_BACKEND = "amqp://guest@localhost:5672//"
+# redis
+CELERY_RESULT_BACKEND = "redis://127.0.0.1/0"
 # 一定要写下面这句，指定时区，否则celery默认使用utc时间，设置的hour会延迟8小时执行
 CELERY_TIMEZONE = 'Asia/Shanghai'
 
